70dec2a252fb5cdd2e9f243525eb38c705622a64
KursonOfficial <kostyabro999@gmail.com>
2025-10-28 23:25:13 +0500

    introduse flag and bomb sprite

--- main.lua
+++ main.lua
@@ -26,16 +26,26 @@ function GM.init()
 	GM.weelY = 0
 	GM.weelVel = .2
 
-	sprite = {numbers = {}, bombs = {}}
-	sprite.numbers = {quad ={}}
+	sprite = {numbers = {}, bombs = {}, flag = {}}
+	sprite.flag.image = love.graphics.newImage("assets/images/flag.png")
 	sprite.numbers.image = love.graphics.newImage("assets/images/numbers.png")
-	sprite.numbers.scaleFactor = GM.Height/(10 * 100) 
+	sprite.bombs.image = love.graphics.newImage("assets/images/bombs.png")
+
+	sprite.numbers.scaleFactor = GM.Height/(10 * 100)
+	sprite.numbers.quad ={}
 	for y = 0 , 1 do
 		for x = 0, 6 do
 			sprite.numbers.quad[x + 1 + y * 7] = lg.newQuad(100 * x, 100 * y, 100, 100, sprite.numbers.image)
 		end
 	end
-	
+
+	sprite.bombs.scaleFactor = GM.Height/(10 * 200)
+	sprite.bombs.quad ={}
+	for y = 0 , 2 do
+		for x = 0, 4 do
+			sprite.bombs.quad[x + 1 + y * 5] = lg.newQuad(200 * x, 200 * y, 200, 200, sprite.bombs.image)
+		end
+	end
 end
 
 function GM.reset()
