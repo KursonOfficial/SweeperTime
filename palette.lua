1c0f68ef7329dc0c41d60665f0dc00a28d895a9e
Cadragonit <juliasheveleva7508@gmail.com>
2025-11-03 01:53:18 +0500

    Add some more themes to the palette
    For now they are just prototypes

--- palette.lua
+++ palette.lua
@@ -47,7 +47,10 @@ cup = function(color) -- Color unpack
 	return color.r, color.g, color.b, color.a
 end
 
-palette = {
+palette = theme0 -- Default theme
+setTheme = function() palette = ocean --[[ Set theme here ]]end
+-- Theme list:
+theme0 = {
 	logoFront    = getColorHS("#CCCCFFFF");
 	hint         = getColorHS("#4C527FFF");
 	versionText  = getColorHS("#333333FF");
@@ -58,3 +61,39 @@ palette = {
 	cellSelectedInner = getColorHS("#4C4C47FF");
 	cellSelectedFrame = getColorHS("#FFFFF233");
 }
+ocean = {
+	logoFront    = getColorHS("#CCCCFFFF");
+	hint         = getColorHS("#4C527FFF");
+	versionText  = getColorHS("#333333FF");
+	debugInfo    = getColorNV(0, 0, 0, 1);
+	cellInner    = getColorHS("#04151FFF");
+	cellRevealed = getColorHS("#66999B44");
+	cellFrame    = getColorHS("#6E889488");
+	cellSelectedInner = getColorHS("#85BAA1FF");
+	cellSelectedFrame = getColorHS("#FFFFF233");
+}
+groovebox = {
+	logoFront    = getColorHS("#CCCCFFFF");
+	hint         = getColorHS("#4C527FFF");
+	versionText  = getColorHS("#333333FF");
+	debugInfo    = getColorHS("#8EC07CFF");
+	cellInner    = getColorHS("#282828FF");
+	cellRevealed = getColorHS("#92837444");
+	cellFrame    = getColorHS("#92837488");
+	cellSelectedInner = getColorHS("#85BAA1FF");
+	cellSelectedFrame = getColorHS("#FFFFF233");
+}
+--[[ Not Ready yet
+everforest = {
+	logoFront    = getColorHS("#CCCCFFFF");
+	hint         = getColorHS("#4C527FFF");
+	versionText  = getColorHS("#333333FF");
+	debugInfo    = getColorNV(0, 0, 0, 1);
+	cellInner    = getColorHS("#1e2326FF");
+	cellRevealed = getColorHS("#2e383cAA");
+	-- cellFrame    = getColorHS("#4f5b5866");
+	cellFrame    = getColorHS("#a7c08034");
+	cellSelectedInner = getColorHS("#4C4C47FF");
+	cellSelectedFrame = getColorHS("#FFFFF233");
+}]]
+setTheme()
