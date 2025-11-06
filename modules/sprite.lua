e62a8b6c6281c846ef213d396c05198a75c1adde
KursonOfficial <kostyabro999@gmail.com>
2025-11-06 20:45:33 +0500

    introduse hearts sprite

--- modules/sprite.lua
+++ modules/sprite.lua
@@ -1,29 +1,42 @@
 
-sprite = {numbers = {}, bombs = {}, flag = {}}
+sprite = {numbers = {},
+		  bombs   = {},
+		  flag    = {},
+		  hearts  = {}}
 
-function sprite.init() 
-	sprite.flag.image = love.graphics.newImage("assets/images/flag.png")
+function sprite.init()
+	sprite.flag.image    = love.graphics.newImage("assets/images/flag.png"   )
 	sprite.numbers.image = love.graphics.newImage("assets/images/numbers.png")
-	sprite.bombs.image = love.graphics.newImage("assets/images/bombs.png")
+	sprite.bombs.image   = love.graphics.newImage("assets/images/bombs.png"  )
+	sprite.hearts.image  = love.graphics.newImage("assets/images/hearts.png" )
 
 	sprite.quads()
 end
 
 function sprite.quads()
-	sprite.flag.scaleFactor = GM.Height/(10 * 200)
+	sprite.flag.scaleFactor    = GM.Height/(10 * 200)
 	sprite.numbers.scaleFactor = GM.Height/(10 * 100)
-	sprite.numbers.quad ={}
+	sprite.bombs.scaleFactor   = GM.Height/(10 * 200)
+	sprite.hearts.scaleFactor  = GM.Height/(5  * 512)
+
+	sprite.numbers.quad = {}
+	sprite.bombs.quad   = {}
+	sprite.hearts.quad  = {}
+
+	--quads for numbers
 	for y = 0 , 1 do
 		for x = 0, 6 do
 			sprite.numbers.quad[x + 1 + y * 7] = lg.newQuad(100 * x, 100 * y, 100, 100, sprite.numbers.image)
 		end
 	end
-
-	sprite.bombs.scaleFactor = GM.Height/(10 * 200)
-	sprite.bombs.quad ={}
+	--quads for bombs
 	for y = 0 , 1 do
 		for x = 0, 4 do
 			sprite.bombs.quad[x + y * 5] = lg.newQuad(200 * x, 200 * y, 200, 200, sprite.bombs.image)
 		end
 	end
+	--quads for hearts
+	sprite.hearts.quad["full"  ] = lg.newQuad(  0, 0, 512, 512, sprite.hearts.image)
+	sprite.hearts.quad["hollow"] = lg.newQuad(512, 0, 512, 512, sprite.hearts.image)
+
 end
