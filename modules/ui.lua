181808bebb52b7da1776f16838e993ad87fac067
Cadragonit <juliasheveleva7508@gmail.com>
2025-12-24 18:52:06 +0500

    Move buttons related stuff to init() and update()

--- modules/ui.lua
+++ modules/ui.lua
@@ -3,11 +3,24 @@ UI = {}
 UI.refreshFonts = function()
 	versionFont       = lg.newFont(GM.Height * 1/72)
 	logoFont          = lg.newFont(GM.Height * 4/45)
-	mainMenuButtons   = lg.newFont(GM.Height * 2/60)
+	MMButtonsFont     = lg.newFont(GM.Height * 2/60)
 	anyButtonHintFont = lg.newFont(GM.Height * 1/36)
 	debugInfoFont     = lg.newFont(GM.Height * 1/60)
 end
 local versionDisplayText = ""
+local MMButtons = {}
+local BUTTON_AMMOUNT = -1
+local GMHUnit = -1
+--[[ NOTE:
+	SEGMENTS and NSEGMENT are needed to define paddings of
+	title and buttons from top and bottom of the screen accordingly.
+	SEGMENTS is the ammount of rows for GM.Height division and
+	NSEGMENT is the index of row you pick from top or bottom
+	(I am aware that this is a wierd solution)
+	.                                           - Cadragonit
+--]]
+local SEGMENTS = 12
+local NSEGMENT = 4
 function UI.init()
 	UI.refreshFonts()
 	bgShader = lg.newShader("assets/missing.glsl")
@@ -17,13 +30,39 @@ function UI.init()
 	bgShader:send("col1", {0, 0, 0, 0.25})
 	bgShader:send("col2", {1, 0, 1, 0.25})
 	versionDisplayText = string.format("SweeperTime %s", GM.version)
+	MMButtons = {
+		{
+			text = "New Game",
+		},
+		{
+			text = "Options",
+		},
+	}
+	BUTTON_AMMOUNT = #MMButtons
+	assert(BUTTON_AMMOUNT >= 1)
 end
-
+local buttons_Y = {}
+local UIButton = {}
 function UI.update()
+	GMHUnit = math.ceil(GM.Height/60) -- GM.Height Unit
 	if     GM.state == "MainMenu" then
+		-- Background
 		bgShader:send("time", love.timer.getTime()*2)
 		bgShader:send("speed", GM.Height/28)
 		bgShader:send("sqare_sz", GM.Height/28)
+		-- Buttons
+		local UIButtonPad = GMHUnit
+		UIButton = {
+			w = logoFont:getWidth("Sweeper Time")*3/4,
+			h = GMHUnit*3,
+		}
+		UIButton.x = (GM.Widht-UIButton.w)/2
+		local BUTTON_BLOCK_HEIGHT = UIButton.h*BUTTON_AMMOUNT + UIButtonPad*(BUTTON_AMMOUNT-1)
+		buttons_Y[1] = GM.Height*((SEGMENTS-NSEGMENT)/SEGMENTS)-BUTTON_BLOCK_HEIGHT/2
+		assert(BUTTON_AMMOUNT >= 1)
+		for i = 2, BUTTON_AMMOUNT do
+			buttons_Y[i] = buttons_Y[i-1] + UIButton.h + UIButtonPad
+		end
 	elseif GM.state == "MainGame" then
 	end
 end
@@ -49,17 +88,6 @@ function UI.printLogo(x, y, speed, amplitude)
 end
 
 function UI.draw()
-	local GMHUnit = math.ceil(GM.Height/60) -- GM.Height Unit
-	--[[ NOTE:
-		SEGMENTS and NSEGMENT are needed to define paddings of
-		title and buttons from top and bottom of the screen accordingly.
-		SEGMENTS is the ammount of rows for GM.Height division and
-		NSEGMENT is the index of row you pick from top or bottom
-		(I am aware that this is a wierd solution)
-		.                                           - Cadragonit
-	--]]
-	local SEGMENTS = 12
-	local NSEGMENT = 4
 	if GM.state == "MainMenu" then
 		-- Background
 		lg.setShader(bgShader)
@@ -83,48 +111,22 @@ function UI.draw()
 		-- Buttons
 		-- TODO:
 		--[[
-			- [ ] Move calculatons to init and update
+			- [X] Move calculatons to init and update
 			- [ ] Make New Game button actually work
 			- [ ] Add cool effects
 		--]]
-		-- CALCULATONS
-		local buttons_as_text = {
-			"New Game",
-			"Options",
-		}
-		local BUTTON_AMMOUNT = #buttons_as_text
-		assert(BUTTON_AMMOUNT >= 1)
-		local UIButtonPad = GMHUnit
-		local UIButton = {
-			w = logoFont:getWidth("Sweeper Time")*3/4,
-			h = GMHUnit*3,
-		}
-		buttons_as_recs = {}
-		local BUTTON_BLOCK_HEIGHT = UIButton.h*BUTTON_AMMOUNT + UIButtonPad*(BUTTON_AMMOUNT-1)
-		buttons_as_recs[1] = Rec.new(
-			(GM.Widht-UIButton.w)/2,
-			GM.Height*((SEGMENTS-NSEGMENT)/SEGMENTS)-BUTTON_BLOCK_HEIGHT/2,
-			UIButton.w,
-			UIButton.h)
-		for i = 2, BUTTON_AMMOUNT do
-			buttons_as_recs[i] = Rec.new(
-				(GM.Widht-UIButton.w)/2,
-				buttons_as_recs[i-1].y + UIButton.h + UIButtonPad,
-				UIButton.w,
-				UIButton.h)
-		end
-		-- DRAWING
-		for i = 1, #buttons_as_recs do
-			butrec = buttons_as_recs[i]
+		for i = 1, BUTTON_AMMOUNT do
+			assert(buttons_Y[i])
+			local butrec = Rec.new(UIButton.x, buttons_Y[i], UIButton.w, UIButton.h)
 			lg.setColor(palette.logoFront.r, palette.logoFront.g, palette.logoFront.b, 0x20/0xFF)
 			drawRec("fill", butrec)
 			love.graphics.setLineWidth(GMHUnit/12)
 			lg.setColor(palette.logoFront.r, palette.logoFront.g, palette.logoFront.b, 1)
 			drawRec("line", butrec)
-			lg.setFont(mainMenuButtons)
-			lg.printf(buttons_as_text[i],
+			lg.setFont(MMButtonsFont)
+			lg.printf(MMButtons[i].text,
 				butrec.x,
-				butrec.y + (butrec.h - mainMenuButtons:getHeight())/2,
+				butrec.y + (butrec.h - MMButtonsFont:getHeight())/2,
 				butrec.w, "center")
 		end
 -- REFACTORING ZONE END -----------------------------------------------------------------
